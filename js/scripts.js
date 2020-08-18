e37b47de3c0936cc458b88b6869b6e86b44a7a3c
Grant Eadie <grantleadie@gmail.com>
2020-08-18 13:14:19 -0700

    will alert based on user input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,29 @@
 $(document).ready(function(){
-  $("form#transportation_survey").submit(function(event){
+  $("form#fortune_teller").submit(function(event) {
     event.preventDefault();
-    $("#work-responses").show();
-    $("input:checkbox[name=work-transportation]:checked").each(function(){
-      const workTransportationMode = $(this).val();
-      $('#work-responses').append(workTransportationMode + "<br>");
+    $("#fortune").show();
+
+    let array = [];
+
+    $("input:checkbox[name=personality]:checked").each(function(){
+      const fortuneAttributes = $(this).val();
+      array.push(fortuneAttributes);
+      });
+
+      let newArray = array.toString();
+      alert(newArray);
+
+        if (newArray === "dogs,cats") {
+          alert("you're an animal person");
+        } else if (newArray === "dogs,cats,rain") {
+          alert("you're a strange person");
+        } else if (newArray === "dogs") {
+          alert("you just like dogs");
+        } else if (newArray === "cats") {
+          alert("you just like cats");
+        } else {
+          alert("you don't like any animals"); 
+        }
+      $('#fortune_teller').hide();
     });
-    $("#fun-responses").show();
-    $("input:checkbox[name=fun-transportation]:checked").each(function(){
-      const funTransportationMode = $(this).val();
-      $('#fun-responses').append(funTransportationMode + "<br>");
-    });
-    $('#transportation_survey').hide();
   });
-});
